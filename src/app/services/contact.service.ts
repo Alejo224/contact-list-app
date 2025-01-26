c97d732d3a69f79d229397fa74292955eaead764
Alejo224 <johantorresalejo19@gmail.com>
2025-01-26 15:12:44 -0500

    feat: Implementa servicio con inyección de dependencias HttpClient y métodos CRUD
    - Añadida inyección de dependencias de HttpClient para la comunicación con el backend.
    - Creado método `list()` para obtener todos los contactos desde la API.
    - Creado método `get(id)` para obtener un contacto por su ID desde la API.
    - Creado método `create(contact)` para crear un nuevo contacto en la API.
    - Creado método `update(id, contact)` para actualizar un contacto existente en la API.
    - Creado método `delete(id)` para eliminar un contacto de la API.
    - Todos los métodos utilizan la URL base de la API backend desarrollada con Spring Boot y Java.

--- src/app/services/contact.service.ts
+++ src/app/services/contact.service.ts
@@ -1,10 +1,39 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactService {
+  //inyectar dependencia
+  private httpClient = inject(HttpClient);
+  
+  // - - - FUNCIONES - - -
+  
+  //listar todos los contactos
+  list (){
+    return this.httpClient.get('http://localhost:8080/api/contacts');
+  }
 
-constructor() { }
+  //obtener contacto por id
+  get (id: number){
+    return this.httpClient.get(`http://localhost:8080/api/contacts/${id}`);
+  }
+
+  //crear contacto
+  create (contact: any){
+    return this.httpClient.post('http://localhost:808/api/contacts/', contact);
+  }
+
+  //actualizar contacto
+  update (id: number, contact: any){
+    return this.httpClient.put(`http://localhost:8080/api/contacts/${id}`, contact);
+  }
+
+  //borrar contacto
+  delete (id: number){
+    return this.httpClient.delete(`http://localhost:8080/api/contacts/${id}`);
+  }
+
 
 }
