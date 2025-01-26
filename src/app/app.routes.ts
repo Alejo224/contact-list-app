c8ab122d497b253523f77ea696504931f920aedb
Alejo224 <johantorresalejo19@gmail.com>
2025-01-26 12:01:04 -0500

    Cambios  y configuración de rutas. app.config : agregamos un provedor de un cliente Http. app.routes: Declarando las ruta raiz que va hacer el componente contact-list.component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    //ruta raiz
+    {
+        path: '',
+        loadComponent: () => import('./contact-list/contact-list.component')
+
+    }
+
+];
