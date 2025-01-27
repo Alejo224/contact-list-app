549bb64100ec0ab3573af9b5370f2bc7caf0d067
Alejo224 <johantorresalejo19@gmail.com>
2025-01-26 23:08:40 -0500

    feat: implementa tabla para listar contactos con diseño responsivo
    - Creada tabla en el componente HTML para mostrar la lista de contactos obtenidos desde el backend.
    - Formato de fecha de creación ajustado a 'dd/MM/yyyy hh:mm a' utilizando Angular pipes.
    - Actualizado el componente `ContactListComponent` para almacenar datos en un array `contacts` con los contactos obtenidos del servicio.
    - Añadido contenedor con clases de Bootstrap para un diseño más ordenado y responsivo.
    - Importado Bootstrap en `styles.css` para aplicar estilos globales.

--- src/app/contact-list/contact-list.component.ts
+++ src/app/contact-list/contact-list.component.ts
@@ -1,20 +1,28 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ContactService } from '../services/contact.service';
+import { DatePipe } from '@angular/common';
+
+
 
 @Component({
   selector: 'app-contact-list',
+  standalone: true,
+  imports: [DatePipe],
   templateUrl: './contact-list.component.html',
   styleUrls: ['./contact-list.component.css']
 })
 export default class ContactListComponent implements OnInit {
 
+  //Almacenar los contactos
+  contacts:any[] = [];
+
   //injectar contactService
   private contactService = inject(ContactService);
 
   ngOnInit(): void{
-    this.contactService.list().subscribe(contacts => {
-      console.log(contacts);
-    })
+    this.contactService.list().subscribe((contacts: any) => {
+      this.contacts = contacts;
+    });
   }
 
 }
